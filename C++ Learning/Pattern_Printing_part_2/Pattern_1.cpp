7289ef6e109350b3908d7c06e515c26937b83508
Kashish <kashishcodework@gmail.com>
2023-09-26 09:58:33 +0530

    Fixed deleted file problem of Pattern Printing

---
+++ C++ Learning/Pattern_Printing_part_2/Pattern_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int row, col, n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for(row =1 ; row <= n; row++){
+        for(col = 1; col <= row; col++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
