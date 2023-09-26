d10fc18c1ef7df5c32df73527088051f0d7f8035
Kashish <kashishcodework@gmail.com>
2023-09-26 12:51:37 +0530

    Solved number pattern questions part 2

--- C++ Learning/Pattern_Printing_part_2/Pattern_1.cpp
+++ C++ Learning/Pattern_Printing_part_2/Pattern_1.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main(){
+    
+    // Pattern to be printed
+    // *
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+    // * * * * * *
+    // * * * * * * * 
+    // * * * * * * * *
+
     int row, col, n;
     cout << "Enter the value of n: ";
     cin >> n;
