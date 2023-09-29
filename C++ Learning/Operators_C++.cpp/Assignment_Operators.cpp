4edcdc73b60208c4ea825429d4408830a29e0624
Kashish <kashishcodework@gmail.com>
2023-09-29 16:46:36 +0530

    Completer Assignment Operators and all Operators

---
+++ C++ Learning/Operators_C++.cpp/Assignment_Operators.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Assignment Operators
+
+    // 1. = Operator
+    int p = 20;
+    cout << p << endl;
+
+    // 2. += Operator
+    int q = 15;
+    q += 7;  // q = q + 7
+    cout << q << endl;
+
+    // 3. -= Operator
+    int r = 7;
+    r -= 5;  // r = r - 5
+    cout << r << endl;
+
+    // 4. *= Operator
+    int s = 9;
+    s *= 2;  // s = s * 2
+    cout << s << endl;
+
+    // 5. / = Operator
+    int t = 24;
+    t /= 12;  // t = t / 12
+    cout << t << endl;
+}
