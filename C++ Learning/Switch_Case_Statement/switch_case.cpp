02a9de607bc89aa9ffd381f9c013172e0d4a5a31
Kashish <kashishcodework@gmail.com>
2023-10-02 18:45:55 +0530

    Learnt Switch Statement

---
+++ C++ Learning/Switch_Case_Statement/switch_case.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Switch - Case Statement
+    // Weekday Number to calculate Weekday Name
+
+    int day;
+    cout << "Enter day: ";
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    }
+}
