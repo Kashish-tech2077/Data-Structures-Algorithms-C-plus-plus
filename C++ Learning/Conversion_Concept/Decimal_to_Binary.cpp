a7009cf9c53ce11886fc29870ceb7cf9fb66ce24
Kashish <kashishcodework@gmail.com>
2023-10-03 13:42:57 +0530

    Leart Conversion of Decimal to Binary

---
+++ C++ Learning/Conversion_Concept/Decimal_to_Binary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Program to convert Decimal Number to Binary Number
+    
+    int num;
+    cout << "Enter the value of num: ";
+    cin >> num;
+    int rem, ans = 0, mul = 1;
+
+    while(num > 0)
+    {
+        // Remainder Part
+        rem = num % 2;  //Also write rem = num&1
+        // Quotient Part
+        num = num / 2;  //Also write num = num >> 1
+        // Answer Part
+        ans = rem*mul+ans;
+        // Multiply Update
+        mul = mul*10;
+    }
+    cout << ans << endl;
+}
