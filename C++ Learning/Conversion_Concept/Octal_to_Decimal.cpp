d93cb1c3310bc016e2ccf1244444cc6fd4a1c271
Kashish <kashishcodework@gmail.com>
2023-10-03 17:13:02 +0530

    Learnt Octal to Decimal Conversion

---
+++ C++ Learning/Conversion_Concept/Octal_to_Decimal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Program to convert Octal to Decimal Number.
+
+    int num;
+    int ans = 0, rem, mul = 1;
+    cout << "Enter Octal Number: ";
+    cin >> num;
+
+    while(num > 0)
+    {
+        // Modulus with 10
+        rem = num % 10;
+        // Divide by 10
+        num = num / 10;
+        // Answer
+        ans = rem * mul + ans;
+        
+        mul *= 8;
+    }
+    cout << ans << endl;
+}
