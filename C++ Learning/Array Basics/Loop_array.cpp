bf05dd974995c09ceb4d6f474eeb26e84e28ce0b
Kashish Pal <kashishcodework@gmail.com>
2023-10-08 19:04:53 +0530

    Learnt basic array and looping through array

---
+++ C++ Learning/Array Basics/Loop_array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string companies[5] = {"Meta", "Google", "Apple", "Microsoft", "Amazon"};
+
+    // For Loop for accessing the elements of arrays
+    for(int i = 0; i <= 4; i++){
+        cout << companies[i] << endl;        
+    }
+
+    // Alternative
+    for(int k = 0; k < 5; k++){
+        cout << k << "(index)-> " << companies[k] << "\n";
+    }
+}
---
+++ C++ Learning/Array Basics/intro_array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+    string cars[5] = {"Ford", "Honda", "Suzuki", "Hyundai","Nissan"};
+
+    cout << cars[2] <<endl;
+    cout << cars[4] << endl;
+}
