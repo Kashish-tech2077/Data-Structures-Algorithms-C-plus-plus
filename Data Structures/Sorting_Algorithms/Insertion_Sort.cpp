5217ea9ffccd5a9b4b5934d12ce786572a9f900c
Kashish Pal <kashishcodework@gmail.com>
2023-10-17 15:11:58 +0530

    Leant Selection Sort Algorithm

---
+++ Data Structures/Sorting_Algorithms/Insertion_Sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+// Program of Insertion Sort Algorithm
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the size of Array: ";
+    cin >> n;
+    // Input the array elements
+    for(int i =0; i<n; i++)
+    cin >> arr[i];
+
+    for(int i = 1; i<n; i++)
+    {
+        int j;
+        for(j = i; j>0; j--)
+        {
+            if(arr[j] < arr[j-1])
+            swap(arr[j], arr[j-1]);
+            else
+            break;
+        }
+    }
+    // Printing the Sorted Array
+    for(int i = 0; i< n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+
+}
